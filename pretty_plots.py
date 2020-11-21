21a4eae1a42c3cf9410e13b53a97f307ae52daa9
Tallivm <30693065+Tallivm@users.noreply.github.com>
2020-11-21 21:14:09 +0200

    Update pretty_plots.py

--- pretty_plots.py
+++ pretty_plots.py
@@ -8,7 +8,7 @@ c_palette = cycle(cm.tab20(linspace(0,1,20)))
 def column_hists(df, ncol=5, square_size=3):
   x, y = int(ceil(len(df.columns)/ncol)), ncol
   fig, axs = plt.subplots(x, y, figsize=(y*square_size, x*square_size))
-  for n, col in zip_longest(x*y, df.columns):
+  for n, col in zip_longest(range(x*y), df.columns):
     x0, x1 = 0+n//ncol, 0+n%ncol
     if col:
       axs[x0, x1].hist(df[col], color=next(c_palette))
