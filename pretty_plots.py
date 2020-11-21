47ddd916557168942c550ede79fdda457a5b1c34
Tallivm <30693065+Tallivm@users.noreply.github.com>
2020-11-21 21:02:13 +0200

    Update pretty_plots.py

--- pretty_plots.py
+++ pretty_plots.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from itertools import cycle
-from numpy import linspace as nplinspace
+from numpy import linspace, ceil
 
-c_palette = cycle(cm.tab20(nplinspace(0,1,20)))
+c_palette = cycle(cm.tab20(linspace(0,1,20)))
 
 def column_hists(df, ncol=5, square_size=3):
-  x, y = int(np.ceil(len(df.columns)/ncol)), ncol
+  x, y = int(ceil(len(df.columns)/ncol)), ncol
   fig, axs = plt.subplots(x, y, figsize=(y*square_size, x*square_size))
   for n, col in enumerate(df.columns):
     x0, x1 = 0+n//ncol, 0+n%ncol
